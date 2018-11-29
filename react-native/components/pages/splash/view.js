a5aaef7dc09c26d369f35ea76e52d3fa6ef3ab8f
daniel <daniel.almeria.nunez@gmail.com>
2018-11-29 18:21:16 +0100

    Add vector-icons

--- react-native/components/pages/splash/view.js
+++ react-native/components/pages/splash/view.js
@@ -3,6 +3,7 @@ import { Text, View, TouchableOpacity } from 'react-native'
 import styles from './styles'
 import i18n from '../../../assets/i18n'
 import { Actions } from 'react-native-router-flux'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 export default class extends PureComponent {
   _onPress = () => Actions.Modal({ title: 'title', message: 'message' })
@@ -11,6 +12,7 @@ export default class extends PureComponent {
     return (
       <View style={styles.container}>
         <TouchableOpacity>
+          <Icon name="ios-person" size={30} color="#4F8EF7" />
           <Text onPress={this._onPress}>{i18n.t('save')}</Text>
         </TouchableOpacity>
       </View>
